30481251e58e06a5a62cc5a245853ed6c933f27a
Jade Larivaille <jadelarivaille@orange.fr>
2025-05-12 19:49:50 +0200

    Rangement des classes

--- ProjetEnsemenc/obj/Debug/net8.0/ProjetEnsemenc.AssemblyInfo.cs
+++ ProjetEnsemenc/obj/Debug/net8.0/ProjetEnsemenc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e490ec5ecaab224e8c72a7a59ec2509091a4e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af35a23f64918babc7c91871e75cceb66376971c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
