979cb8eb2d764f56fd73a844133419d8b8861ce7
Jade Larivaille <jadelarivaille@orange.fr>
2025-05-23 10:26:04 +0200

    pour switch de branche

--- ProjetEnsemenc/obj/Debug/net8.0/ProjetEnsemenc.AssemblyInfo.cs
+++ ProjetEnsemenc/obj/Debug/net8.0/ProjetEnsemenc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233af92984f7af6429ab6ca23fd124cafa5db420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac5c2101baa5f97fa6bb4eb76f56bc0105b3420")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetEnsemenc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
